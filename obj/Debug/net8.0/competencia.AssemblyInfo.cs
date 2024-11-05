63a077b0ca6c07a52080dcf819a80a9a233ce5b2
Michhoow <michaelmartinez1604@gmail.com>
2024-11-05 00:58:01 -0400

    Cambios ligeros como en los colores etc, nada importante.

--- obj/Debug/net8.0/competencia.AssemblyInfo.cs
+++ obj/Debug/net8.0/competencia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("competencia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ae8d7f473419ed9812aaf874a7082b7d2d3900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c0234ffe4aced7eec7067e1a7a6408a55474b19")]
 [assembly: System.Reflection.AssemblyProductAttribute("competencia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("competencia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
